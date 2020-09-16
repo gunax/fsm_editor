ce765b1eb29eb92a920e6da4479fd8055d156776
GitHub Action <action@github.com>
2020-09-16 09:20:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/gunax/fsmeditor/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gunax/fsmeditor/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gunax.fsmeditor.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gunax.fsmeditor.MyBundle
 
 class MyApplicationService {
 
